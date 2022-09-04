613c9b4cb891832eb77c38da5574de044698959d
Alexey <105539363+Hoobbaaboobba@users.noreply.github.com>
2022-09-04 16:26:28 +0300

    Add files via upload

--- calculator.js
+++ calculator.js
@@ -1,5 +1,4 @@
 const priceInput = document.querySelector('[name=height]');
-const quantityInput = document.querySelector('[name=width]');
 const thirdInput =  document.querySelector('[name=lenght]');
 const total = document.querySelector('.total');
 const quantityLabel = document.querySelector('.quantity-label');
@@ -8,7 +7,6 @@ const quantityLabel = document.querySelector('.quantity-label');
 // create the functions that we'll need
 function calculatePieCost() {
     const price = priceInput.value;
-    const quantity = quantityInput.value;
     const third = thirdInput.value;
     const cost = price * 1 * third * 450;
     total.innerText = '' + cost.toFixed(2);
@@ -20,5 +18,4 @@ calculatePieCost();
 
 // add our event listeners
 priceInput.addEventListener('input', calculatePieCost);
-quantityInput.addEventListener('input', calculatePieCost);
 thirdInput.addEventListener('input', calculatePieCost);
--- calculator2.js
+++ calculator2.js
@@ -8,6 +8,7 @@ const quantityLabels = document.querySelector('.quantity-label');
 // create the functions that we'll need
 function calculatePieCosts() {
     const prices = priceInputs.value;
+    const quantitys = quantityInputs.value;
     const thirds = thirdInputs.value;
     const costs = prices * 1 * thirds * 5500;
     totals.innerText = '' + costs.toFixed(2);
